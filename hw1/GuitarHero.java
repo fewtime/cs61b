2e17c3ed54fa93a372998e28deee77265e97bcf5
cowlog <cowlog.li@gmail.com>
2019-06-23 16:40:10 +0800

    [Add] hw1

---
+++ hw1/GuitarHero.java
@@ -0,0 +1,39 @@
+import synthesizer.GuitarString;
+
+public class GuitarHero {
+    private static final String KEYBOARD = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+
+    public static void main(String[] args) {
+        GuitarString[] concert = new GuitarString[KEYBOARD.length()];
+
+        for (int i = 0; i < concert.length; ++i) {
+            concert[i] = new GuitarString(440.0 * Math.pow(2, (i - 24.0) / 12.0));
+        }
+
+        while (true) {
+
+            /* check if the user has typed a key; if so, process it */
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                int index = KEYBOARD.indexOf(key);
+                if (index != -1) {
+                    concert[index].pluck();
+                }
+            }
+
+            /* compute the superposition of samples */
+            double sample = 0;
+            for (int i = 0; i < concert.length; ++i) {
+                sample += concert[i].sample();
+            }
+
+            /* play the sample on standard audio */
+            StdAudio.play(sample);
+
+            /* advance the simulation of each guitar string by one step */
+            for (int i = 0; i < concert.length; ++i) {
+                concert[i].tic();
+            }
+        }
+    }
+}
--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -1,5 +1,8 @@
 package synthesizer;
 import org.junit.Test;
+
+import java.util.Iterator;
+
 import static org.junit.Assert.*;
 
 /** Tests the ArrayRingBuffer class.
@@ -9,7 +12,36 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<String> arb = new ArrayRingBuffer<>(3);
+
+        assertEquals(3, arb.capacity());
+
+        assertTrue(arb.isEmpty());
+        assertEquals(0, arb.fillCount());
+
+        arb.enqueue("apple");
+        arb.enqueue("banana");
+
+        assertEquals(2, arb.fillCount());
+
+        assertFalse(arb.isFull());
+
+        assertEquals("apple", arb.dequeue());
+
+        arb.enqueue("cake");
+        arb.enqueue("duck");
+
+        Iterator<String> seer = arb.iterator();
+        assertTrue(seer.hasNext());
+        assertEquals("banana", seer.next());
+        assertEquals("cake", seer.next());
+        assertEquals("duck", seer.next());
+        assertFalse(seer.hasNext());
+
+        assertEquals("banana", arb.peek());
+
+        assertEquals(3, arb.fillCount());
+        assertTrue(arb.isFull());
     }
 
     /** Calls tests for ArrayRingBuffer. */
